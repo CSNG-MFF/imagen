c8a4f09c37e8bf59e2d5836489f07c3a2c206efb
antolikjan <antolikjan@gmail.com>
2020-02-22 16:43:00 +0100

    Update random.py
    Getting rid of oldnumeric dependencies

--- imagen/random.py
+++ imagen/random.py
@@ -5,7 +5,7 @@ __version__='$Revision$'
 
 import numpy
 
-from numpy import zeros,floor,where,choose,less,greater,random_array
+from numpy import zeros,floor,where,choose,less,greater
 
 import param
 from param.parameterized import ParamOverrides
@@ -204,15 +204,18 @@ class RandomDotStereogram(PatternGenerator):
         # Choose random colors and locations of square dots
         random_seed = p.random_seed
 
-        random_array.seed(random_seed*12,random_seed*99)
-        col=where(random_array.random((ndots))>=0.5, 1.0, -1.0)
+        
+        np.random.seed(random_seed*12+random_seed*99)
+        col=np.where(np.random.random((ndots))>=0.5, 1.0, -1.0)
 
-        random_array.seed(random_seed*122,random_seed*799)
-        xpos=floor(random_array.random((ndots))*(bigxsize+2*dotsize)) - halfdot
+        np.random.seed(random_seed*122+random_seed*799)
+        xpos=np.floor(np.random.random((ndots))*(bigxsize+2*dotsize)) - halfdot
 
-        random_array.seed(random_seed*1243,random_seed*9349)
-        ypos=floor(random_array.random((ndots))*(bigysize+2*dotsize)) - halfdot
-
+        np.random.seed(random_seed*1243+random_seed*9349)
+        ypos=np.floor(np.random.random((ndots))*(bigysize+2*dotsize)) - halfdot
+        
+        
+        
         # Construct arrays of points specifying the boundaries of each
         # dot, cropping them by the big image size (0,0) to (bigxsize,bigysize)
         x1=xpos.astype(numpy.int) ; x1=choose(less(x1,0),(x1,0))
